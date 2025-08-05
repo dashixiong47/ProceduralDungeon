268b92af57ea868e733ac5e8ad17ba47d0861119
dashixiong47 <475896756@qq.com>
2025-08-05 12:51:30 +0800

    添加point

---
+++ Source/ProceduralDungeonEditor/Private/EditorMode/Point.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Point.h"
+
+#include "EditorModeManager.h"
+#include "ProceduralDungeonEdModeToolkit.h"
+#include "SProceduralDungeonEdModeWidget.h"
+
+
+// Sets default values
+APoint::APoint()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bIsEditorOnlyActor = true;
+}
+
+// Called when the game starts or when spawned
+void APoint::BeginPlay()
+{
+	Super::BeginPlay();
+	FProceduralDungeonEdMode* EdMode=GetProceduralEdMode();
+	TSharedPtr<FModeToolkit> MToolkit = EdMode->GetToolkit();
+	TSharedPtr<FProceduralDungeonEdModeToolkit> Toolkit = StaticCastSharedPtr<FProceduralDungeonEdModeToolkit>(MToolkit);
+	TSharedPtr<class SWidget> Widget=Toolkit->GetInlineContent();
+	TSharedRef<SProceduralDungeonEdModeWidget> MyWidget = StaticCastSharedRef<SProceduralDungeonEdModeWidget>(Toolkit->GetInlineContent().ToSharedRef());
+ 
+}
+
+// Called every frame
+void APoint::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
+
+FProceduralDungeonEdMode* APoint::GetProceduralEdMode()
+{
+	FEditorModeTools& ModeTools = GLevelEditorModeTools();
+	return static_cast<FProceduralDungeonEdMode*>(ModeTools.GetActiveMode(FProceduralDungeonEdMode::EM_ProceduralDungeon));
+}
---
+++ Source/ProceduralDungeonEditor/Private/EditorMode/Point.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProceduralDungeonEdMode.h"
+#include "GameFramework/Actor.h"
+#include "Point.generated.h"
+
+UCLASS()
+class PROCEDURALDUNGEONEDITOR_API APoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APoint();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	FProceduralDungeonEdMode* GetProceduralEdMode();
+};
